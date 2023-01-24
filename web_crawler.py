92e6489a5ae5fb52cdcf605a23103e135e5e92be
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-24 20:23:22 +0800

    fix: add try except function

--- web_crawler.py
+++ web_crawler.py
@@ -5,13 +5,16 @@ import pandas as pd
 initDate = datetime.today()
 totalData = list()
 while True:
-    rawData = pd.read_json("https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json")
-    totalData.append(rawData)
-    print(datetime.today())
-    if str(datetime.today()).split(" ")[0] == str(initDate + timedelta(days = 1)).split(" ")[0]:
-        totalData = pd.concat(totalData, axis = 0).drop_duplicates()
-        totalData.to_excel(f"{str(datetime.today())}.xlsx", index = None)
-        initDate = datetime.today()
-        totalData = list()
-    else:
-        time.sleep(30)
+    try:
+        rawData = pd.read_json("https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json")
+        totalData.append(rawData)
+        print(datetime.today())
+        if str(datetime.today()).split(" ")[0] == str(initDate + timedelta(days = 1)).split(" ")[0]:
+            totalData = pd.concat(totalData, axis = 0).drop_duplicates()
+            totalData.to_excel(f"{str(datetime.today())}.xlsx", index = None)
+            initDate = datetime.today()
+            totalData = list()
+        else:
+            time.sleep(30)
+    except:
+        continue
